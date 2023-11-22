33ee7cdfffd756c51f0e8f61cacb0cae1f268757
Alexandr Zaharov <a.zakharov@aknaucer.ru>
2023-11-22 17:26:59 +0500

    Adaptive

--- index.js
+++ index.js
@@ -25,4 +25,19 @@ const slider2 = new Swiper(".products__slider", {
 	},
 });
 
+const cross = document.querySelector('.cross');
+const crossTop = cross.querySelector('.cross__top');
+const crossMiddle = cross.querySelector('.cross__middle');
+const crossBottom = cross.querySelector('.cross__bottom');
+const menu = document.querySelector('.menu');
 
+const activateMenu = () => {
+	crossTop.classList.toggle('cross__top--click');
+	crossMiddle.classList.toggle('cross__middle--click');
+	crossBottom.classList.toggle('cross__bottom--click');
+	menu.classList.toggle('menu--open')
+}
+
+cross.addEventListener('click', () => {
+	activateMenu();
+});
