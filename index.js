b51af281b6066da76c1f814c50d1ef4ab61093a6
Alexandr Zaharov <a.zakharov@aknaucer.ru>
2023-11-23 12:25:54 +0500

    Adds animate link

--- index.js
+++ index.js
@@ -41,3 +41,24 @@ const activateMenu = () => {
 cross.addEventListener('click', () => {
 	activateMenu();
 });
+
+
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+	link.addEventListener('click', function(e) {
+		e.preventDefault();
+		let href = this.getAttribute('href').substring(1);
+		const scrollTarget = document.getElementById(href);
+
+		//const topOffset = document.querySelector('.scrollto').offsetHeight;
+		const topOffset = 0;
+		const elementPosition = scrollTarget.getBoundingClientRect().top;
+		const offsetPosition = elementPosition - topOffset;
+
+		window.scrollBy({
+			top: offsetPosition,
+			behavior: 'smooth'
+		});
+	});
+});
