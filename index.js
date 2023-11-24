88629951fcc023468a64da1f40ab7f37ee8ddc94
Alexandr Zaharov <a.zakharov@aknaucer.ru>
2023-11-24 14:52:41 +0500

    Fix

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const controls = new Swiper(".hello__controls", {
-	slidesPerView: 3,
-});
-
-const slider = new Swiper(".hello__slider", {
-	loop: true,
-	scrollbar: {
-		el: ".swiper-scrollbar",
-	},
-	thumbs: {
-		swiper: controls,
-	},
-});
+// const controls = new Swiper(".hello__controls", {
+// 	slidesPerView: 3,
+// });
+//
+// const slider = new Swiper(".hello__slider", {
+// 	loop: true,
+// 	scrollbar: {
+// 		el: ".swiper-scrollbar",
+// 	},
+// 	thumbs: {
+// 		swiper: controls,
+// 	},
+// });
 
 
 const slider2 = new Swiper(".products__slider", {
